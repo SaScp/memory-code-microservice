690bbb232ac7012007135cd4e907afb169862859
Belyashik2K <lovelybelyashik@gmail.com>
2024-04-13 11:18:21 +0300

    fix

--- memories-service/config.py
+++ memories-service/config.py
@@ -5,6 +5,10 @@ class Settings(BaseSettings):
     YANDEXGPT_SERVICE_URL: str
     USER_SERVICE_URL: str
     
+    DISCOVERY_HOST_CLIENT: str
+    DISCOVERY_HOST: str
+    DISCOVERY_PORT: int
+    
     model_config: SettingsConfigDict = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8"
--- memories-service/src/eureka.py
+++ memories-service/src/eureka.py
@@ -1,6 +1,8 @@
 from py_eureka_client.eureka_client import EurekaClient
 
+from ..config import config
+
 async def register_eureka():
-    client = EurekaClient(eureka_server="http://${DISCOVERY_HOST}:${DISCOVERY_PORT}/eureka/",
+    client = EurekaClient(eureka_server=f"http://{config.DISCOVERY_HOST_CLIENT}:{config.DISCOVERY_PORT}/eureka/",
                           app_name="memories-service")
     await client.start()
