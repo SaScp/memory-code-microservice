8a31ecbf65dc8bf58fc5de4014f06b294ec8ed20
Alexander Vorobev <3048453@gmail.com>
2024-04-14 00:54:06 +0300

    fix

--- discovery-service/src/main/java/ru/memorycode/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/ru/memorycode/discoveryservice/DiscoveryServiceApplication.java
@@ -1,9 +1,6 @@
 package ru.memorycode.discoveryservice;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
